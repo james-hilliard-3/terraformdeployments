cd33645c2fc252420fd4f096227d794c6524715e
james-hilliard-3 <42178698+james-hilliard-3@users.noreply.github.com>
2024-05-02 17:13:41 -0700

    Update firewall.tf

--- firewall.tf
+++ firewall.tf
@@ -1,13 +1,11 @@
-# Create a public IP for Azure Firewall
 resource "azurerm_public_ip" "fw_public_ip" {
   name                = "fw-public-ip"
   location            = azurerm_resource_group.hub_rg.location
   resource_group_name = azurerm_resource_group.hub_rg.name
   allocation_method   = "Static"
-  sku                 = "Standard"
+  sku                 = "Standard"  # Corrected from 'sku_name' to 'sku'
 }
 
-# Azure Firewall
 resource "azurerm_firewall" "hub_firewall" {
   name                = "example-firewall"
   location            = azurerm_resource_group.hub_rg.location
@@ -18,6 +16,9 @@ resource "azurerm_firewall" "hub_firewall" {
     subnet_id            = azurerm_subnet.fw_subnet.id
     public_ip_address_id = azurerm_public_ip.fw_public_ip.id
   }
+
+  sku_name = "AZFW_VNet"
+  sku_tier = "Standard"
 
   threat_intel_mode = "Alert"
 }
