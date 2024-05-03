22ef752657ac9934a062d36964f5eff80952a918
james-hilliard-3 <42178698+james-hilliard-3@users.noreply.github.com>
2024-05-02 17:15:05 -0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,9 @@
+terraform {
+  required_version = ">= 0.14"
+}
+
 provider "azurerm" {
   features {}
+  skip_provider_registration = true
+  environment = var.environment  # This now references the declared variable
 }
